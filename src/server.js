6abeda1b2d83073258b607f9025ab946d2cdb7ba
Natalia <onishenko2@gmail.com>
2026-01-20 22:05:01 +0100

    hw1

---
+++ src/server.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import pinoHttp from 'pino-http';
+
+dotenv.config();
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+app.use(pinoHttp());
+
+//routes
+app.get('/notes', (req, res) => { res.status(200).json({ message: 'Retrieved all notes' }); });
+app.get('/notes/:noteId', (req, res) => { res.status(200).json({ message: `Retrieved note wits ID: ${noteId}` }); });
+app.get('/test-error', () => { throw new Error('Simulated server error'); });
+
+//Error 404
+app.use((req, res) => { res.status(404).json({ message: 'Route not found' }); });
+
+//Error 500
+app.use((err, req, res, next) => { res.status(500).json({ message: err.message }); });
+
+const PORT = Number(process.env.PORT) || 3000;
+
+app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); });
